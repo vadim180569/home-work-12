5168ee384241284e951362b99805ea4c7b082e8a
vadim180569 <vadim9771505001@gmail.com>
2024-11-20 19:06:09 +0300

    Add files via upload

---
+++ home work 12.py
@@ -0,0 +1,12 @@
+am_hw = 12
+nu_hs = 1.5
+name = 'Course: Python, '
+ti_gob = nu_hs / am_hw
+num = 'всего задач: 12, '
+num1 = 'затрачено часов: 1.5, '
+num2 = 'среднее время выполнения: '+ str(ti_gob) + ' часа. '
+print(name + num + num1 + num2)
+
+
+
+
